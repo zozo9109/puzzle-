329891cc81f38d1a81b7874e5a753eb3e5514914
zozo9109@naver.com <zozo9109@naver.com>
2020-04-05 19:13:04 +0900

    퍼즐 맞추기

--- 퍼즐게임맞추기 게임.cpp
+++ 퍼즐게임맞추기 게임.cpp
@@ -1,20 +1,100 @@
-﻿// 퍼즐게임맞추기 게임.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
-//
+﻿#include<Bangtal.h>
+#pragma comment(lib,"Bangtal.lib")
 
-#include <iostream>
+	 
+SceneID scene1;
+ObjectID startButton,endButton, restartButton, Original, zoro1, zoro2, zoro3, zoro4, zoro5, zoro6, zoro7, zoro8, zoro9, blank;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+
+
+
+
+int boardX[3] = {
+	210, 410, 610
+};
+
+int boardY[3] = {
+	500, 300, 100
+};
+
+
+int blankX;
+int blankY;
+
+
+
+
+ObjectID createObject(const char* name, const char* image, SceneID scene, int x, int y, bool shown) {
+	ObjectID object = createObject(name, image);
+	locateObject(object, scene, x, y);
+	if(shown == true)
+		if (shown) {
+			showObject(object);
+		}
+	return object;
+		}
 }
 
-// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
-// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
+void mouseCallback(ObjectID object, int x, int y, MouseAction action) {
+	if (object == endButton) {
+		endGame();
+	}
+	else if
+}
+for (int i = 0; i < 3; i++) {
+	for (int i = 0; i < 3; i++){
+		if (object == board[i][j]){
+			if (i == 0 && j == 0){
+				if (board[i][j + 1] == blank){
 
-// 시작을 위한 팁: 
-//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
-//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
-//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
-//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
-//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
-//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
+
+
+
+
+
+					
+
+
+
+
+
+
+	
+Swap(boardX,)
+
+
+
+					Swap(board[i][j], blank);
+
+					SwapX(boardX[i][j], boardX[i + 1][j]);
+
+					SwapY(boardY[i][j], boardY[i + 1][j]);
+
+
+
+
+
+
+
+
+
+int main() {
+	
+	setMouseCallback(mouseCallback);
+	
+	scene1 = createScene("퍼즐맞추기", "Original.jpg");
+
+	scene2 = createScene("흰화면", "background.jpg");
+
+	startButton = createObject("시작버튼", "start.png", scene1, 280, 70, true);
+
+	endButton = createObject("종료버튼", "end.png", scene1, 280, 20, true);
+
+	restartButton = createObject("재시작버튼,"reatart.png", scene1, true);
+
+
+
+
+
+	startGame(scene1);
+}
